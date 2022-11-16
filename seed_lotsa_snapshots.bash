b8886acbf89246855d3191e1739f601cf0866eaf
Eli Reisman <elireisman@github.com>
2022-11-16 15:06:55 -0800

    add total time to serial seed script; todo: fan out these writes too + reduce batch sizes a bit more

--- seed_lotsa_snapshots.bash
+++ seed_lotsa_snapshots.bash
@@ -2,23 +2,28 @@
 
 set -euo pipefail
 
-# assumed `make cassandra` was already run and the instance is ready
+# assumes `make cassandra` was already run and the instance is ready
 
 if [ ! -f bin/seed ]; then
   make build
 fi
 
-# seed a bunch of small snapshots with a canonical and a couple historicals
-for s in {1..100}; do
-  bin/seed -c -s 5 -m 20 -d 80
-done
 
-# seed a bunch of medium-sized snapshots with a canonical and a few historicals
-for s in {1..100}; do
-  bin/seed -c -s 3 -m 40 -d 120
-done
+function seed_data {
+  # seed a bunch of small snapshots with a canonical and a couple historicals
+  for s in {1..100}; do
+    bin/seed -c -s 5 -m 20 -d 80
+  done
 
-# seed a few big ass snapshots with a canonical and a couple historicals
-for s in {1..10}; do
-  bin/seed -c -s 3 -m 100 -d 1000
-done
+  # seed a bunch of medium-sized snapshots with a canonical and a few historicals
+  for s in {1..100}; do
+    bin/seed -c -s 3 -m 40 -d 120
+  done
+
+  # seed a few big ass snapshots with a canonical and a couple historicals
+  for s in {1..10}; do
+    bin/seed -c -s 3 -m 100 -d 1000
+  done
+}
+
+time seed_data
